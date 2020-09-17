580a7ee972a612cad3be0f66dd53d1c6aca4884c
kellypowers <kpowers.nj@gmail.com>
2020-09-17 14:25:19 -0400

    Done.

--- test/root.js
+++ test/root.js
@@ -1,18 +1,18 @@
-global.expect = require('expect');
-global.cheerio = require('cheerio');
+// global.expect = require('expect');
+// global.cheerio = require('cheerio');
 
-const fs = require('fs');
-const jsdom = require('mocha-jsdom');
-const path = require('path');
-const babel = require('babel-core');
+// const fs = require('fs');
+// const jsdom = require('mocha-jsdom');
+// const path = require('path');
+// const babel = require('babel-core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf8');
+// const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf8');
 
-const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
-  presets: ['es2015']
-});
+// const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+//   presets: ['es2015']
+// });
 
-jsdom({
-  html,
-  src: babelResult.code
-});
+// jsdom({
+//   html,
+//   src: babelResult.code
+// });
