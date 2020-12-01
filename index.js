c90f073fb302ee820f5f27efe877c71bce06a742
Sharon Watkins <swatkins876@gmail.com>
2020-12-01 12:08:50 +0000

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,37 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+const issuesWithUpdatedApiUrl = issues.map(function (issue) {
+  return Object.assign({}, issue, {
+    url: issue.url.replace(/api/g, "api-v2")
+  });
+});
+
+const total = function (startValue, item) {
+  return startValue + item.comments_count;
+};
+
+const commentCountAcrossIssues = issues.reduce(total, 0);
+
+const openIssues = issues.reduce((openIssues, issue) => {
+  if (issue.state == 'open') {
+    return [...openIssues, issue];
+  }
+  return openIssues;
+}, []);
+
+const nonAutomaticIssues = issues.reduce((nonAutomaticIssues, issue) => {
+  if (issue.body != 'This pull request has been automatically created by learn.co.') {
+    return [...nonAutomaticIssues, issue];
+  }
+  return nonAutomaticIssues;
+}, []);
+
+const htmlResults = document.getElementById('results');
+htmlResults.innerHTML = nonAutomaticIssues.map(issue => `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+    </tr>`
+  ).join('');
