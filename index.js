7cb9364fc94cd71bf6da152dae90b9a1eee3d9be
github username <github email address>
2021-02-09 20:17:36 -0800

    Done.

--- index.js
+++ index.js
@@ -9009,3 +9009,31 @@ const issuesWithUpdatedApiUrl = issues
 
 const commentCountAcrossIssues=issues.map(issue=>
  issue.comments_count).reduce((acc,currentValue)=>acc+currentValue,0)
+
+ const openIssues = issues.reduce((openIssues, issue) => {
+  if (issue.state === 'open') {
+    return [...openIssues, issue];
+  }
+
+  return openIssues;
+}, []);
+
+const nonAutomaticIssues = issues.reduce((totalIssues, issue) => {
+  const isAutomaticIssue = issue.body.includes('automatically created by learn.co');
+
+  if (!isAutomaticIssue) {
+    totalIssues.push(issue);
+  }
+
+  return totalIssues;
+}, []);
+
+const $tbody = document.getElementById('results');
+$tbody.innerHTML = nonAutomaticIssues
+  .map(issue => `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+    </tr>`
+  )
+  .join('');
